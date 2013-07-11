6b1d2dd8fb6bbe20dbaca853da7234dfcfd8208c
Ricardo Cervantes <ricardo07@gmail.com>
2013-07-11 11:41:24 -0500

    Add tax calculations on server side, add image to each item, return order as a list after submitting, handle server being down.

---
+++ public/interaction.js
@@ -0,0 +1,104 @@
+// alert("Javascript is loaded.");
+
+function serverIsDown() {
+  $('.panel h2').text('Our server is having difficulties, please try again...');
+  $('input').removeAttr("disabled");
+}
+
+function assembleOrder(arrayOfItems) {
+  orderHTML = "<div><ol>";
+  for (i=0; i < JSON.parse(arrayOfItems).length; i++) {
+    if (JSON.parse(arrayOfItems)[i]["name"] == "Please select a drink") continue;
+    orderHTML += "<li>" + JSON.parse(arrayOfItems)[i]["name"] + "<//li>";
+  }
+  orderHTML += "</ol></div>";
+  return orderHTML;
+}
+
+function loadImage(selectedItem) {
+  $('#picture img').attr('src', selectedItem.val() + ".jpg");
+}
+
+
+$(document).ready(function() {
+  $("input[type=submit]").attr("disabled", "disabled");
+
+  // --------- Adding drinks to order ---------
+  $('form').on('change', 'select', function() {
+    // If I set the item to 'Please select your coffee', I want to get rid of it.
+    if ((this).value == "Please select a drink"){
+      $(this).empty();
+      $(this).parent().remove();
+    }
+    // ---------------------------
+
+    // Load the picture of the selected item
+    loadImage($("option:selected",this));
+    
+    // -----------------------------------
+
+    // Count the number of select elements in the page that have no drink selected
+    empty_select_elements = 0;
+    $("select").each(function (index, element) {
+      if (element.value == "Please select a drink")
+        empty_select_elements += 1;
+    });
+    // ----------------------------
+
+    // Count the number of drinks
+    number_of_drinks = $("select").length;
+    $('#drinks').text(number_of_drinks);
+    // --------------------------
+
+    // Calculate the total of the order
+    total = 0; // Total invoice in cents
+    $("option:selected[data-price]").each(function(){
+        total += parseInt($(this).attr('data-price'));
+    });
+    $("#cost").text("$ " + (total/100.0).toFixed(2));
+    $("input[type=submit]").removeAttr("disabled");
+    // ----------------------------------
+
+    // If there are multiple select elements in the page, clone the select element
+    if (empty_select_elements < 1) {
+      dropdown_element_html = $('form div').first().html(); // get the HTML code from the div inside the from
+      // $('form').append('<div>' + dropdown_element_html + '</div>');
+      $('<div>' + dropdown_element_html + '</div>').insertAfter('form div:last'); // This inserts the div right after the last one, not at the end of the panel.
+    };
+    // --------------------------------------
+
+    //Change the tax calculations
+    $('#taxes').load('/taxes', $('form').serialize());
+    // --------------------------
+    
+  });
+
+  // ------- Submit logic ----------
+  $("form").submit(function(event) {
+    event.preventDefault();
+    $("input").attr("disabled", "disabled");
+    //Change the tax calculations
+    $('#taxes').load('/taxes', $('form').serialize());
+    
+    d = $("form").serialize();
+
+    // Server request
+    $.ajax({
+      type: "POST",
+      url: "/shop",
+      data: d,
+      success: function(data) {
+        $("div.panel h2").text("Your order is being processed...");
+        $("div.panel form div").remove();
+        $("div.panel form").prepend(assembleOrder(data));
+      },
+      error: function(data) {
+        serverIsDown(); // This is the part that handles the error being down
+      },
+      dataType: "html"
+    });
+    // -----------------------------
+  });
+
+  
+});
--- shop.rb
+++ shop.rb
@@ -6,7 +6,34 @@ get '/' do
   erb :index
 end
 
-post '/shop' do
-  'Thanks for your order'
+get '/taxes' do
+  # how many drinks * 100 cents
+  "$ " + (Array(params[:drink]).length-1).to_s + ".00"
 end
 
+post '/shop' do
+  # Both of these instructions are passed onto the server console
+  # puts "Receiving order"
+  # puts params.inspect
+
+  content_type :json
+  
+  response = [] 
+
+  params[:drink].each do |drink|
+    response << { 
+      name: drink
+    }
+  end
+
+  response.to_json
+
+end
+
+post '/signup' do
+  puts "\n" * 3
+  puts "Received email: #{params[:email]}"
+  puts "\n" * 3
+
+  "Thanks for signing up!"
+end
